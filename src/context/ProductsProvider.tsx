990f87ea6e1ce7ea1c77e6c6fb3017001b9a6a82
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-17 21:00:36 -0300

    added context

---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,61 @@
+import { ReactElement, createContext, useState, useEffect } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+
+const initState: ProductType[] = [];
+
+// const initState: ProductType[] = [
+//   {
+//     sku: "item0001",
+//     name: "Widget",
+//     price: 9.99,
+//   },
+//   {
+//     sku: "item0002",
+//     name: "Premium Widget",
+//     price: 19.99,
+//   },
+//   {
+//     sku: "item0003",
+//     name: "Deluxe Widget",
+//     price: 29.99,
+//   },
+// ];
+
+export type UseProductsConstextType = { products: ProductType[] };
+
+const initContextState: UseProductsConstextType = { products: [] };
+
+const ProductContext = createContext<UseProductsConstextType>(initContextState);
+
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const ProductProvider = ({ children }: ChildrenType): ReactElement => {
+  const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("http://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
+  
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductContext;
